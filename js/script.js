e53035846e7c614c04029b277da4e2e02456c9b8
Bartosz Barc <bartosz.barc721@gmail.com>
2023-01-08 19:04:33 +0100

    changed JS

--- js/script.js
+++ js/script.js
@@ -1,51 +1,65 @@
-let inCurrencyElement = document.querySelector(".js-inCurrency")
-let outCurrencyElement = document.querySelector(".js-outCurrency")
-let amountElement = document.querySelector(".js-amount")
-let formElement = document.querySelector(".js-form")
-let resultElement = document.querySelector(".js-result")
+{
+    let inCurrencyElement = document.querySelector(".js-inCurrency")
+    let outCurrencyElement = document.querySelector(".js-outCurrency")
+    let amountElement = document.querySelector(".js-amount")
+    let formElement = document.querySelector(".js-form")
 
-let plnToUsd = 0.2284286
-let eurToUsd = 1.06855
-let gbpToUsd = 1.20519
-let amountToUsd
-let result = 0
-let rateToUsd
-let usdToRate
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
 
-    switch (inCurrencyElement.value) {
-        case "USD":
-            rateToUsd = 1
-            break;
-        case "PLN":
-            rateToUsd = plnToUsd
-            break;
-        case "EUR":
-            rateToUsd = eurToUsd
-            break;
-        case "GBP":
-            rateToUsd = gbpToUsd
+    const plnToUsd = 0.2284286
+    const eurToUsd = 1.06855
+    const gbpToUsd = 1.20519
+
+    const calculateAmoutToUsd = () => {
+        let rateToUsd
+        switch (inCurrencyElement.value) {
+            case "USD":
+                rateToUsd = 1
+                break;
+            case "PLN":
+                rateToUsd = plnToUsd
+                break;
+            case "EUR":
+                rateToUsd = eurToUsd
+                break;
+            case "GBP":
+                rateToUsd = gbpToUsd
+
+        }
+        const amountToUsd = amountElement.value * rateToUsd
+
+        return amountToUsd
+    }
+
+    const calculateUsdToResulte = (amountToUsd) => {
+        let usdToRate
+        switch (outCurrencyElement.value) {
+            case "USD":
+                usdToRate = 1
+                break;
+            case "PLN":
+                usdToRate = plnToUsd
+                break;
+            case "EUR":
+                usdToRate = eurToUsd
+                break;
+            case "GBP":
+                usdToRate = gbpToUsd
+
+        }
+        const result = amountToUsd / usdToRate;
+        return result
+    }
+    const showResult = (result) => {
+        const resultElement = document.querySelector(".js-result")
+        return resultElement.innerText = `${amountElement.value} ${inCurrencyElement.value} = ${result.toFixed(2)} ${outCurrencyElement.value}`
 
     }
-    amountToUsd = amountElement.value * rateToUsd
-
-    switch (outCurrencyElement.value) {
-        case "USD":
-            usdToRate = 1
-            break;
-        case "PLN":
-            usdToRate = plnToUsd
-            break;
-        case "EUR":
-            usdToRate = eurToUsd
-            break;
-        case "GBP":
-            usdToRate = gbpToUsd
-
+    const init = () => {
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            showResult(calculateUsdToResulte(calculateAmoutToUsd()));
+        })
     }
-    result = amountToUsd / usdToRate
-    resultElement.innerText = `${amountElement.value} ${inCurrencyElement.value} = ${result.toFixed(2)} ${outCurrencyElement.value}`
-
-})
+    init();
+}
